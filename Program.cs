a27c1c5c816af3e98c373dd2dd4d089e727f5693
Yosha Kunnummal <yoshakunnummal@rocketmortgage.com>
2021-11-03 14:59:22 -0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BranchGroupExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
